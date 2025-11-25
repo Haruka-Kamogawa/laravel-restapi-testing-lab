430ff3d944014909a1c51066f65b6cdd57d3f571
Haruka Kamogawa <yachuanxuan@gmail.com>
2025-11-25 10:52:15 +0800

    first commit

---
+++ app/Models/Todo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'is_done',
+    ];
+
+    protected $casts = [
+        'is_done' => 'boolean',
+    ];
+}
